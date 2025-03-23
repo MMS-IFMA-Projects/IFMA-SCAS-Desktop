494c585535c3ae654564b71f7283c2b837e6efd5
Mayron-Martins <mayronmartins013@gmail.com>
2025-03-23 17:54:54 -0300

    Estruturação de Componentes

---
+++ IFMA SCAS Desktop/View/Login.cs
@@ -0,0 +1,15 @@
+namespace IFMA_SCAS_Desktop
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
