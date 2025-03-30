da3dd054a0291af63eb90dc8e762d9f37df2df87
Robson Luan <robsonluan273@hotmail.com>
2025-03-30 15:15:28 -0300

    Setting connection

--- IFMA SCAS Desktop/Database/Connection.cs
+++ IFMA SCAS Desktop/Database/Connection.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Database
 {
-    public class Connection
+    public class Connection : DbContext
     {
+        public Connection(DbContextOptions options) : base(options)
+        {
+        }
 
+        protected Connection()
+        {
+        }
+    
+        public DbSet<Entities.User> Users { get; set; }
     }
 }
--- IFMA SCAS Desktop/Program.cs
+++ IFMA SCAS Desktop/Program.cs
@@ -1,3 +1,5 @@
+using Npgsql;
+
 namespace IFMA_SCAS_Desktop
 {
     internal static class Program
@@ -12,6 +14,8 @@ namespace IFMA_SCAS_Desktop
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Login());
+
+            
         }
     }
 }
