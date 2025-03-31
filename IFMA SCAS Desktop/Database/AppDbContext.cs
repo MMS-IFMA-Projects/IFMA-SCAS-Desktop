945e669d80251c44ad9d58544ba8146f05d85980
Robson Luan <robsonluan273@hotmail.com>
2025-03-31 20:03:53 -0300

    Connection is working
    Fix JSON reading

--- IFMA SCAS Desktop/Database/AppDbContext.cs
+++ IFMA SCAS Desktop/Database/AppDbContext.cs
@@ -2,6 +2,14 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 
+/*
+    URI: postgresql://postgres:d7Lxtw6E1yI0gXFa@hardily-jointed-stud.data-1.use1.tembo.io:5432/postgres
+    Hostname: hardily-jointed-stud.data-1.use1.tembo.io
+    Porta: 5432
+    Username: postgres
+    Password: d7Lxtw6E1yI0gXFa
+ */
+
 public class AppDbContext : DbContext
 {
     protected readonly IConfiguration configuration;
@@ -13,9 +21,21 @@ public class AppDbContext : DbContext
     }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        /**/
+        /*
         string connectionString = this.configuration.GetConnectionString("DBConnection");
         connectionString = string.Join(";", connectionString);
+        */
+
+        string[] connectionArgs = {
+            "Host=hardily-jointed-stud.data-1.use1.tembo.io",
+            //"Server=:5432/postgres",
+            "Port=5432",
+            "Database=sas",
+            "User Id=postgres",
+            "Password=d7Lxtw6E1yI0gXFa" 
+        };
+
+        string connectionString = string.Join(";", connectionArgs);
 
         optionsBuilder.UseNpgsql(connectionString);
     }
