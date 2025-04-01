0d9ce0f42a44911fb4371790a509f2919fbf6baf
Mayron-Martins <mayronmartins013@gmail.com>
2025-03-31 23:01:22 -0300

    Correção Schema

--- IFMA SCAS Desktop/Entities/User.cs
+++ IFMA SCAS Desktop/Entities/User.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
+    [Table("user", Schema = "public")]
     public class User
     {
-        [Key]
         public uint id { get; set; }
         public uint usertypeid { get; set; }
         public string name { get; set; }
